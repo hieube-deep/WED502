11c5f0874da8e7444dabf32bfe6c6ba3a614b537
hieube-deep <nguyentrunghieu11032006@gmail.com>
2026-01-09 18:39:49 +0700

    lap 2

---
+++ lap2/main.js
@@ -0,0 +1,55 @@
+"use strict";
+// Object.defineProperty(exports, "__esModule", { value: true });
+var Rate;
+(function (Rate) {
+    Rate["low"] = "Th\u1EA5p";
+    Rate["medium"] = "Trung b\u00ECnh";
+    Rate["hight"] = "Cao";
+})(Rate || (Rate = {}));
+const Products = [
+    {
+        name: "Áo thun",
+        price: 199000,
+        sale: true,
+        rate: Rate.medium
+    },
+    {
+        name: "Quần jean",
+        price: 399000,
+        sale: false,
+        rate: Rate.hight
+    },
+    {
+        name: "Giày sneaker",
+        price: 899000,
+        sale: true,
+        rate: Rate.low
+    }
+];
+const ListProduct = () => {
+    const table = document.querySelector('table');
+    Products.map((item, index) => {
+        return table.innerHTML += `
+         <tr>
+            <td >${index + 1}</td>
+            <td>${item.name}</td>
+            <td>${item.price}</td>
+            <td>${item.rate}</td>
+            <td>${item.sale}</td>
+        </tr>
+        `;
+    });
+};
+ListProduct();
+const addDescription = (products) => {
+    return products.map(item => ({
+        ...item,
+        description: item.price > 5 ? 'Tốt' : 'Bình thường'
+    }));
+};
+const productsWithDesc = addDescription(Products);
+const totalPrice = (products) => {
+    return products.reduce((sum, item) => sum + item.price, 0);
+};
+console.log("Tổng giá sản phẩm:", totalPrice(Products));
+//# sourceMappingURL=main.js.map
---
+++ lap2/main.ts
@@ -0,0 +1,68 @@
+enum Rate {
+  low = 'Thấp',
+  medium = 'Trung bình',
+  hight = 'Cao'
+}
+
+type Product = {
+  name: string;
+  price: number;
+  sale: boolean;
+  rate: Rate;
+};
+const Products:Product[]=[
+     {
+    name: "Áo thun",
+    price: 199000,
+    sale: true,
+    rate: Rate.medium
+  },
+  {
+    name: "Quần jean",
+    price: 399000,
+    sale: false,
+    rate: Rate.hight
+  },
+  {
+    name: "Giày sneaker",
+    price: 899000,
+    sale: true,
+    rate: Rate.low
+  }
+];
+
+const ListProduct = ()=>{
+
+    const table = document.querySelector('table') as HTMLTableElement;
+    Products.map((item,index)=>{
+       return table.innerHTML+=`
+         <tr>
+            <td >${index+1}</td>
+            <td>${item.name}</td>
+            <td>${item.price}</td>
+            <td>${item.rate}</td>
+            <td>${item.sale}</td>
+        </tr>
+        `
+    });
+}
+ListProduct();
+
+type ProductWithDesc = Product & {
+  description: string;
+};
+
+const addDescription = (products: Product[]): ProductWithDesc[] => {
+  return products.map(item => ({
+    ...item,
+    description: item.price > 5 ? 'Tốt' : 'Bình thường'
+  }));
+};
+const productsWithDesc = addDescription(Products);
+
+
+const totalPrice = (products: Product[]): number => {
+  return products.reduce((sum, item) => sum + item.price, 0);
+};
+
+console.log("Tổng giá sản phẩm:", totalPrice(Products));
